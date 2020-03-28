f486e529ee3fac5e0d2418a50104824cc9ba4c7c
pontakornth <pontakorn61@gmail.com>
2020-03-28 20:07:40 +0700

    feat: Add base style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /*
-** TailwindCSS Configuration File
-**
-** Docs: https://tailwindcss.com/docs/configuration
-** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
-*/
+ ** TailwindCSS Configuration File
+ **
+ ** Docs: https://tailwindcss.com/docs/configuration
+ ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
+ */
+
+import plugin from 'tailwindcss/plugin'
+
 module.exports = {
   theme: {},
   variants: {},
-  plugins: []
+  plugins: [
+    plugin(({ addBase, config }) => {
+      addBase({
+        h1: { fontSize: config('theme.fontSize.2xl') },
+        h2: { fontSize: config('theme.fontSize.xl') },
+        h3: { fontSize: config('theme.fontSize.lg') },
+        a: {
+          color: config('theme.colors.blue.500'),
+          textDecoration: 'underline'
+        }
+      })
+    })
+  ]
 }
